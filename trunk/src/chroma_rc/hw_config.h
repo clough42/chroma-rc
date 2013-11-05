55e4dad90b7e6a6c778d2e9b277d43de2284233d
clough42@gmail.com <clough42@gmail.com@1de6c351-7421-d689-1516-f28559583e31>
2013-11-05 15:54:26 +0000

    Initial sketch, with hardware configuration.

---
+++ trunk/src/chroma_rc/hw_config.h
@@ -0,0 +1,41 @@
+#include <Arduino.h>
+
+#ifndef _HW_CONFIG_H_
+#define _HW_CONFIG_H_
+
+/* RGB Channel 1 */
+#define PIN_RGB1_R A2
+#define PIN_RGB1_G A1
+#define PIN_RGB1_B A3
+
+/* RGB Channel 2 */
+#define PIN_RGB2_R 12
+#define PIN_RGB2_G 13
+#define PIN_RGB2_B 11
+
+/* RGB Channel 3 */
+#define PIN_RGB3_R 5
+#define PIN_RGB3_G 6
+#define PIN_RGB3_B 4
+
+/* RGB Channel 4 */
+#define PIN_RGB4_R 9
+#define PIN_RGB4_G 8
+#define PIN_RGB4_B 10
+
+/* Mono Channels */
+#define PIN_MONO_X A0
+#define PIN_MONO_Y 7
+
+/* Servos */
+#define PIN_SERVO_1 2
+#define PIN_SERVO_2 3
+
+/* Configuration Jumpers */
+#define PIN_JP1 A7
+#define PIN_JP2 A6
+
+/* Battery Voltage Monitoring */
+#define PIN_BATT_V A5
+
+#endif
