3545c10ddf6f51e1514d9564dc2fb5b7a6f1a4f6
clough42@gmail.com <clough42@gmail.com@1de6c351-7421-d689-1516-f28559583e31>
2013-11-17 16:32:00 +0000

    Split up files into cpp/h for main and hardware.

---
+++ trunk/src/chroma_rc/hardware.cpp
@@ -0,0 +1,27 @@
+#include "hardware.h"
+#include "hw_config.h"
+
+void initialize_hardware()
+{
+  pinMode(PIN_RGB1_R, OUTPUT);
+  pinMode(PIN_RGB1_G, OUTPUT);
+  pinMode(PIN_RGB1_B, OUTPUT);
+  pinMode(PIN_RGB2_R, OUTPUT);
+  pinMode(PIN_RGB2_G, OUTPUT);
+  pinMode(PIN_RGB2_B, OUTPUT);
+  pinMode(PIN_RGB3_R, OUTPUT);
+  pinMode(PIN_RGB3_G, OUTPUT);
+  pinMode(PIN_RGB3_B, OUTPUT);
+  pinMode(PIN_RGB4_R, OUTPUT);
+  pinMode(PIN_RGB4_G, OUTPUT);
+  pinMode(PIN_RGB4_B, OUTPUT);
+  pinMode(PIN_MONO_X, OUTPUT);
+  pinMode(PIN_MONO_Y, OUTPUT);
+  pinMode(PIN_SERVO_1, INPUT);
+  pinMode(PIN_SERVO_2, INPUT);
+  pinMode(PIN_JP1, INPUT);
+  pinMode(PIN_JP2, INPUT);
+  pinMode(PIN_BATT_V, INPUT);
+}
+
+
---
+++ trunk/src/chroma_rc/hardware.h
@@ -0,0 +1,6 @@
+#ifndef _HARDWARE_H_
+#define _HARDWARE_H_
+
+void initialize_hardware();
+
+#endif // _HARDWARE_H_
